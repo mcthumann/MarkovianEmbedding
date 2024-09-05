ddc27d5cad614816e60ada0faa64a2436e4675ad
Cole-Thumann <cc_thumann@coloradocollege.edu>
2024-09-05 08:49:53 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is the main file for a program that  will simulate the full hydrodynamic theory
+# of short time brownian motion by embedding the memory dependent kernal into Markovian
+# states
+
+def run():
+    print("Markovian Embedding")
+
+
+if __name__ == '__main__':
+    run()
